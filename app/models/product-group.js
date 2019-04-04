e6de8a83fdda24493d33ffdd761dee99162849b7
Aad Versteden <madnificent@gmail.com>
2019-04-04 14:06:15 +0200

    Import models from webshop
    These models were copied from the webshop.  It could make sense to
    share them through a shared addon at some point in time.  For now,
    we're okay with just copying the code around.

---
+++ app/models/product-group.js
@@ -0,0 +1,17 @@
+import { computed } from '@ember/object';
+import DS from 'ember-data';
+const { Model } = DS;
+import { attr, belongsTo, hasMany } from '@ember-decorators/data';
+
+export default class ProductGroupModel extends Model {
+  @attr() label;
+  @attr('number') sortIndex;
+  @belongsTo('product-group', { inverse: 'childGroups' }) parentGroup;
+  @hasMany('product-group', { inverse: 'parentGroup' }) childGroups;
+  @hasMany('product') spotlightProducts;
+
+  @computed('childGroups.@each.sortIndex')
+  get sortedChildren() {
+    return (this.childGroups || []).sortBy('sortIndex');
+  }
+}
