24c6f76d142ad2a8f3c8fa296c5b386e0f86fc8f
Aad Versteden <madnificent@gmail.com>
2019-06-15 20:16:57 +0200

    Better transitions and rendering for product-group card
    The card did not transition nicely.  Getting these nested transitions
    right seems to be very hard so I assume we're not really there yet.
    However, it does feel correct at this time.

--- app/components/product-group-card.js
+++ app/components/product-group-card.js
@@ -2,15 +2,65 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
 import fade from 'ember-animated/transitions/fade';
+import scale from 'ember-animated/motions/scale';
+import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
+import move from 'ember-animated/motions/move';
+import { wait } from 'ember-animated';
 
 export default class ProductGroupCardComponent extends Component {
+  @action
+  sortedChildren() {
+    /* do nothing */
+  }
+
+  emptyArary = []
+
+  /* -- Open state -- */
   @tracked isOpen = false
 
+  headerTransition = function*( { insertedSprites, removedSprites, duration }) {
+    const [ insertedSprite ] = insertedSprites;
+    const [ removedSprite ] = removedSprites;
+
+    removedSprite.endAtSprite( insertedSprite );
+    insertedSprite.startAtSprite( removedSprite );
+
+    for ( const sprite of [ insertedSprite, removedSprite ] ) {
+      scale( sprite, duration );
+      move( sprite, duration );
+    }
+
+    insertedSprite.hide();
+
+    fadeOut( removedSprite, 2 * duration / 3 );
+    yield wait( duration / 2 );
+    insertedSprite.reveal();
+    fadeIn( insertedSprite, 2 * duration / 3 );
+  }
+
+  /* -- editing state -- */
+
+  @tracked
+  editing = false;
+
   @action
-  sortedChildren(){
-    // console.log("sorting children");
-    // console.log(args);
+  toggleEditing(){
+    this.editing = !this.editing;
   }
 
   transition = fade;
+
+  /* -- editing functionality -- */
+  @action
+  update() {
+    this.args.productGroup.save();
+  }
+
+  @action
+  undo() {
+    this.args.productGroup.rollbackAttributes();
+  }
+
+  /* -- creation functionality -- */
+
 }
