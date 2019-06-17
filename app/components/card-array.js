3de106a95cb885c8b126069de04fa51a3158a589
Aad Versteden <madnificent@gmail.com>
2019-06-17 15:26:06 +0200

    Fix card styling
    Cards are now styled more correctly.  The changes can best be seen as
    fixes to the styling.

--- app/components/card-array.js
+++ app/components/card-array.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 
 export default class CardArrayComponent extends Component {
   get cardColorClass() {
-    return `card-array-card card-array-${this.args.color}`;
+    return `card-array card-array-${this.args.color}`;
   }
 
   colors = ["dark", "medium", "light"]
