9aa6248dddc8ff05d75f4541c58338f02efff2b0
Aad Versteden <madnificent@gmail.com>
2019-06-22 14:24:27 +0200

    Creating animatad-header component
    This component provides an animated header for a card, smoothly
    transitioning between the small and large title.

---
+++ app/components/animated-header.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import headerTransition from 'frontend-freddie/utils/transitions/header-transition';
+
+export default class AnimatedHeaderComponent extends Component {
+  headerTransition = headerTransition
+}
