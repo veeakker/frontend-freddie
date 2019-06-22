83d4f1c480f9d84399691aaf4f992736fff9533a
Aad Versteden <madnificent@gmail.com>
2019-06-22 16:24:14 +0200

    Use AnimatedHeader in ProductGroupCard
    The ProductGroupCard, like many other places, uses an animated
    header.  We're now using the shared component.
    
    Some changes to the components have been made in order to make things
    easier to understand or to extend functionality so these components
    behave the way we want them to in our situation.

--- app/utils/transitions/header-transition.js
+++ app/utils/transitions/header-transition.js
@@ -3,17 +3,32 @@ import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
 import move from 'ember-animated/motions/move';
 import { wait } from 'ember-animated';
 
-export default function*( { insertedSprites, removedSprites, duration }) {
-  const [ insertedSprite ] = insertedSprites;
-  const [ removedSprite ] = removedSprites;
+export default function*( { insertedSprites, removedSprites, keptSprites, duration }) {
+  const [ insertedSprite, ...otherInsertedSprites ] = insertedSprites;
+  const [ removedSprite, ...otherRemovedSprites ] = removedSprites;
+
+  let keptSprite, otherKeptSprites = [];
+  if( keptSprites.length > 0 ) {
+    [ keptSprite, ...otherKeptSprites ] = keptSprites;
+  }
 
   removedSprite.endAtSprite( insertedSprite );
   insertedSprite.startAtSprite( removedSprite );
 
-  for ( const sprite of [ insertedSprite, removedSprite ] ) {
-    scale( sprite, duration );
-    move( sprite, duration );
+  for ( const sprite of [ insertedSprite, removedSprite, keptSprite ] ) {
+    if( sprite ) { // keptSprite might be undefined
+      scale( sprite, duration );
+      move( sprite, duration );
+    }
   }
+
+  for (const sprite of [...otherInsertedSprites, ...otherKeptSprites] ) {
+    sprite.startTranslatedBy({dx: removedSprite.initialBounds.width, dy: 0});
+    move( sprite );
+    fadeIn( sprite );
+  }
+  for (const sprite of otherRemovedSprites )
+    fadeOut( sprite );
 
   insertedSprite.hide();
 
