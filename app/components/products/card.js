4f5dd754f0942364980f5f62dfa31d075c4577ac
Aad Versteden <madnificent@gmail.com>
2019-06-23 14:42:09 +0200

    Allow name and index of product to be updated
    The name and the index of a product can now be updated when rendering
    the product itself.

--- app/components/products/card.js
+++ app/components/products/card.js
@@ -1,4 +1,14 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import fade from 'ember-animated/transitions/fade';
 
 export default class ProductsCardComponent extends Component {
+  transition = fade
+
+  @action
+  updateProduct(event){
+    event.preventDefault();
+
+    this.args.product.save();
+  }
 }
