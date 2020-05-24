3171b8550b38557073c6780590c303025f421bc5
Aad Versteden <madnificent@gmail.com>
2020-05-24 15:58:03 +0200

    Add support for setting icons
    Icons (named labels) can now be set in the interface.  Work on the
    shop frontend to render these has also been executed.

--- app/components/products/input-form.js
+++ app/components/products/input-form.js
@@ -5,7 +5,40 @@ import Component from '@glimmer/component';
 
 export default class ProductsInputFormComponent extends Component {
   @service store
-  
+
+  get labelArray() {
+    const enabled = (this.args.product.productLabels || []);
+    return [{ uri: "http://veeakker.be/product-labels/d9fa5ad6-0d0e-4990-b8a7-ca3a60eb3a85",
+        label: "Frozen",
+        image: "/product-labels/diepvries.png",
+        selected: enabled.includes( "http://veeakker.be/product-labels/d9fa5ad6-0d0e-4990-b8a7-ca3a60eb3a85")
+      },
+      { uri: "http://veeakker.be/product-labels/fa0d5d40-762e-4f89-ac82-46c1a4ee00bf",
+        label: "Natuurpunt",
+        image: "/product-labels/natuurpunt.png",
+        selected: enabled.includes( "http://veeakker.be/product-labels/fa0d5d40-762e-4f89-ac82-46c1a4ee00bf")
+      },
+      { uri: "http://veeakker.be/product-labels/c9e43e38-3f7f-4116-9817-80bdede3f123",
+        label: "PintaFish",
+        image: "/product-labels/pintafish.png",
+        selected: enabled.includes( "http://veeakker.be/product-labels/c9e43e38-3f7f-4116-9817-80bdede3f123")
+      }];
+  }
+
+  @action
+  setLabel(label, value) {
+    const currentLabels = this.args.product.productLabels || [];
+    if( value ) {
+      this.args.product.set(
+        "productLabels",
+        [ label.uri, ...currentLabels ] );
+    } else {
+      this.args.product.set(
+        "productLabels",
+        [ ...currentLabels.filter( (x) => x !== label.uri ) ] );
+    }
+  }
+
   @action
   submit(event){
     event.preventDefault();
