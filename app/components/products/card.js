f14600990351df5515abb91d9403bd5846f8e197
Aad Versteden <madnificent@gmail.com>
2020-11-22 22:40:47 +0100

    Slightly nicer opening and closing of cards
    Cards can now open and close a tad nicer than what they could before.
    This should help with the understanding of the application.

--- app/components/products/card.js
+++ app/components/products/card.js
@@ -1,11 +1,25 @@
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
-import fade from 'ember-animated/transitions/fade';
+import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
+import { wait } from 'ember-animated';
 
 export default class ProductsCardComponent extends Component {
-  transition = fade
+  *transition( {insertedSprites, removedSprites, duration} ) {
+    for( const sprite of insertedSprites ) {
+      fadeIn( sprite );
+    }
+
+    for( const sprite of removedSprites ) {
+      fadeOut( sprite, { duration: 4 * duration / 5, to: 0.8 });
+    }
+
+    yield wait( duration * 4 / 5 );
+    for( const sprite of removedSprites ) {
+      fadeOut( sprite, { duration: duration / 5, to: 0 } );
+    }
+
+  }
 
   @service store
 
--- app/components/simple-animated-card.js
+++ app/components/simple-animated-card.js
@@ -1,6 +1,4 @@
 import Component from '@glimmer/component';
-import fade from 'ember-animated/transitions/fade';
 
 export default class SimpleAnimatedCardComponent extends Component {
-  transition = fade;
 }
