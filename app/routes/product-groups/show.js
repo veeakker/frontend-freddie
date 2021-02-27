9ebec8bdf613fc8f38485487e61a973af83f2f41
Aad Versteden <madnificent@gmail.com>
2021-02-27 11:46:35 +0100

    Fix for pagination on products of ProductGroup
    The shop didn't always render the desired content correctly.  When there
    were too many products in a paginated group, only the first N products
    were shown where the intention was to render all of them.

--- app/routes/product-groups/show.js
+++ app/routes/product-groups/show.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default class ProductGroupsShowRoute extends Route {
+  async model( {product_group_id} ){
+    const data = await this.store.query(
+      'product-group',
+      {"filter[:id:]": product_group_id,
+       "page[size]": 250,
+       "include": "products" }
+    );
+
+    return data.firstObject;
+  }
 }
